cfa693f0bd571bb7921cf65888497cc56f128dbd
cyonits <cyonitss@gmail.com>
2023-08-25 17:57:52 +0900

    [BE] Init : 스프링 초기설정 (#1)

---
+++ BE/src/test/java/shop/petmily/PetmilyApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.petmily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetmilyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
