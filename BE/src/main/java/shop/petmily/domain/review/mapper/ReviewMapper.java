607758d91565d643ba1806db77f399577d9e5fb3
45hyewon <haewon0611@gmail.com>
2023-08-28 21:01:48 +0900

    [BE] Review 수정 (#10)

--- BE/src/main/java/shop/petmily/domain/review/mapper/ReviewMapper.java
+++ BE/src/main/java/shop/petmily/domain/review/mapper/ReviewMapper.java
@@ -1,10 +1,12 @@
 package shop.petmily.domain.review.mapper;
 
+import org.mapstruct.Mapper;
 import shop.petmily.domain.review.Dto.ReviewPatchDto;
 import shop.petmily.domain.review.Dto.ReviewPostDto;
 import shop.petmily.domain.review.Dto.ReviewResponseDto;
 import shop.petmily.domain.review.entity.Review;
 
+@Mapper(componentModel = "Spring")
 public interface ReviewMapper {
     Review reviewPostToReview(ReviewPostDto reviewPostDto);
     Review reviewPatchToReview(ReviewPatchDto reviewPatchDto);
