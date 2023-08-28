82e009c9980d60ad26a932dac95c6f51dae0ba8e
45hyewon <haewon0611@gmail.com>
2023-08-28 20:28:15 +0900

    [BE] Reservation CRUD 기본 클래스 구현 (#9)

--- BE/src/main/java/shop/petmily/domain/reservation/mapper/ReservationMapper.java
+++ BE/src/main/java/shop/petmily/domain/reservation/mapper/ReservationMapper.java
@@ -1,4 +1,14 @@
 package shop.petmily.domain.reservation.mapper;
 
+import shop.petmily.domain.reservation.dto.ReservationPatchDto;
+import shop.petmily.domain.reservation.dto.ReservationPostDto;
+import shop.petmily.domain.reservation.dto.ReservationResponseDto;
+import shop.petmily.domain.reservation.entity.Reservation;
+
 public interface ReservationMapper {
+    Reservation reservationPostDtoToReservation(ReservationPostDto reservationPostDto);
+
+    Reservation reservationPatchDtoToReservation(ReservationPatchDto reservationPatchDto);
+
+    ReservationResponseDto reservationToReservationResponseDto(Reservation reservation);
 }
--- BE/src/main/java/shop/petmily/domain/reservation/repository/ReservationRepository.java
+++ BE/src/main/java/shop/petmily/domain/reservation/repository/ReservationRepository.java
@@ -1,4 +1,6 @@
 package shop.petmily.domain.reservation.repository;
 
-public interface ReservationRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReservationRepository extends JpaRepository {
 }
