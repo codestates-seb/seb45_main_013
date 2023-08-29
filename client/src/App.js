879c1b266a809452883f449cb121df3f0db52250
tedhun21 <songjihun940409@gmail.com>
2023-08-29 10:59:23 +0900

    [FE] Init: Tailwind Custom Theme (#37)

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-fontWeight-400 underline text-gray00 text-black px-8 ">Hello world!</h1>
     </div>
   );
 }
