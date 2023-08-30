b08c411b1ef50df17d045f9bbd923fe270d684a1
tedhun21 <songjihun940409@gmail.com>
2023-08-30 09:19:53 +0900

    [FE] Init: stylelint 설정 (#37)

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,13 +1,5 @@
-import styled from 'styled-components';
-
-const Container = styled.div`
-  border: 1px solid blue;
-  height: 100px;
-  width: 100px;
-`;
-
 const Home = () => {
-  return <Container>home</Container>;
+  return <div>home</div>;
 };
 
 export default Home;
