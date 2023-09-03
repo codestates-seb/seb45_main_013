5482aa5ad872503fb3417c7488f13123e7fd909c
Dohee Kim <kdhwh09@gmail.com>
2023-09-04 01:11:29 +0900

    [FE] fe: Joy UI 설정

--- client/src/pages/EditUserProfile.tsx
+++ client/src/pages/EditUserProfile.tsx
@@ -6,6 +6,11 @@ import Checkbox from '@mui/material/Checkbox';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import FormControlLabel from '@mui/material/FormControlLabel';
 
+import Accordion from '@mui/joy/Accordion';
+import AccordionDetails from '@mui/joy/AccordionDetails';
+import AccordionGroup from '@mui/joy/AccordionGroup';
+import AccordionSummary from '@mui/joy/AccordionSummary';
+
 // 보호자 or 펫시터 ?  (API)
 
 const EditTitile = styled.div`
@@ -24,6 +29,12 @@ const EditUserProfile = () => {
       <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
         Sign In
       </Button>
+      <AccordionGroup>
+        <Accordion>
+          <AccordionSummary>Title</AccordionSummary>
+          <AccordionDetails>Content</AccordionDetails>
+        </Accordion>
+      </AccordionGroup>
     </>
   );
 };
