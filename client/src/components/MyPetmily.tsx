9e9462cf605ec1ee0a59788995be4dc2a8d6a640
Dohee Kim <kdhwh09@gmail.com>
2023-09-14 16:04:12 +0900

    [FE] Feat: Mypage 수정 & ViewJournal 작성중

--- client/src/components/MyPetmily.tsx
+++ client/src/components/MyPetmily.tsx
@@ -9,8 +9,10 @@ import Card from '@mui/joy/Card';
 import CardContent from '@mui/joy/CardContent';
 import CardOverflow from '@mui/joy/CardOverflow';
 import Divider from '@mui/joy/Divider';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 
-// 펫 이미지 없을 때 크기 수정
+// 펫 이미지 없을 때 디폴트 이미지 수정
+// 펫밀리 카드 디자인 수정
 
 const BucketUrl = process.env.REACT_APP_BUCKET_URL || '';
 const apiUrl = process.env.REACT_APP_API_URL;
@@ -45,56 +47,65 @@ const MyPetmily = () => {
         <Text>나의 Petmily</Text>
         {petmily.length > 0 && (
           <Link to="/mypage/register">
-            <PetsButton>
-              <img src="imgs/Plus.svg" alt="AddPets" />
-            </PetsButton>
+            <PlusIcon />
           </Link>
         )}
       </TextContainer>
 
-      {petmily.length > 0 ? (
-        petmily.map((pet) => (
-          <Card key={pet.petId} variant="outlined" sx={{ width: '100%', mb: '32px' }}>
-            <CardOverflow>
-              <AspectRatio ratio="2">
-                {pet.photo ? (
-                  <img src={pet.photo.replace(/https:\/\/bucketUrl/g, BucketUrl)} alt="pet" />
-                ) : pet.type === 'CAT' ? (
-                  <img src="imgs/CatProfile.png" alt="Cat" />
-                ) : (
-                  <img src="imgs//PetProfile.png" alt="Dog" />
-                )}
-              </AspectRatio>
-              <Link to={`/mypage/${pet.petId}/edit`}>
-                <PetsButton>
-                  <img src="imgs/Edit.svg" alt="EditPets" />
-                </PetsButton>
-              </Link>
-            </CardOverflow>
-            <CardContent>
-              <PetInfo>{pet.name}</PetInfo>
-            </CardContent>
-            <CardOverflow variant="soft">
-              <Divider inset="context" />
-              <CardContent orientation="horizontal">
+      <CardsContainer>
+        {petmily.length > 0 ? (
+          petmily.map((pet) => (
+            <Card key={pet.petId} variant="outlined" sx={{ width: '80%', mb: '32px' }}>
+              <CardOverflow>
+                <AspectRatio ratio="2">
+                  {pet.photo ? (
+                    <img src={pet.photo.replace(/https:\/\/bucketUrl/g, BucketUrl)} alt="pet" />
+                  ) : pet.type === 'CAT' ? (
+                    <img src="imgs/CatProfile.png" alt="Cat" />
+                  ) : (
+                    <img src="imgs//PetProfile.png" alt="Dog" />
+                  )}
+                </AspectRatio>
+                {/* <Link to={`/mypage/${pet.petId}/edit`}>
+                  <PetsButton>
+                    <img src="imgs/Edit.svg" alt="EditPets" />
+                  </PetsButton>
+                </Link> */}
+              </CardOverflow>
+
+              <CardContent sx={{ display: 'flex', justifyContent: 'space-between' }}>
                 <PetInfo>
-                  {pet.age}살 / {pet.weight}kg
+                  {pet.name} [{pet.species}]
                 </PetInfo>
+                <PetInfo>{pet.male ? '남자아이' : '여자아이'}</PetInfo>
               </CardContent>
-            </CardOverflow>
-          </Card>
-        ))
-      ) : (
-        <NoPetsContainer>
-          <div>등록된 펫밀리가 없습니다.</div>
-          <div>프로필을 등록하면 빠른 예약이 가능해요!</div>
-          <Link to="/mypage/register">
-            <Button variant="contained" sx={{ backgroundColor: '#279eff', mt: 5 }}>
-              등록하러 가기
-            </Button>
-          </Link>
-        </NoPetsContainer>
-      )}
+
+              <CardOverflow variant="soft">
+                <Divider inset="context" />
+
+                <CardContent orientation="horizontal">
+                  <PetInfo>
+                    {pet.age}살 / {pet.weight}kg
+                  </PetInfo>
+                  <Link to={`/mypage/${pet.petId}/edit`}>
+                    <Button>수정하기</Button>
+                  </Link>
+                </CardContent>
+              </CardOverflow>
+            </Card>
+          ))
+        ) : (
+          <NoPetsContainer>
+            <div>등록된 펫밀리가 없습니다.</div>
+            <div>프로필을 등록하면 빠른 예약이 가능해요!</div>
+            <Link to="/mypage/register">
+              <Button variant="contained" sx={{ backgroundColor: '#279eff', mt: 5 }}>
+                등록하러 가기
+              </Button>
+            </Link>
+          </NoPetsContainer>
+        )}
+      </CardsContainer>
     </PetmilyContainer>
   );
 };
@@ -109,12 +120,20 @@ const PetmilyContainer = styled.div`
 const TextContainer = styled.div`
   display: flex;
   justify-content: space-between;
+  align-items: center;
+  margin-bottom: 30px;
 `;
 
 const Text = styled.div`
-  margin-bottom: 30px;
   font-weight: 900;
-  ${(props) => props.theme.fontSize.s18h27};
+  font-size: 18px;
+`;
+
+const CardsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
 `;
 
 const PetsButton = styled.button`
@@ -139,7 +158,7 @@ const PetInfo = styled.div`
   margin-top: 4px;
   color: ${(props) => props.theme.textColors.gray00};
   font-weight: ${(props) => props.theme.fontWeights.bold};
-  ${(props) => props.theme.fontSize.s14h21};
+  ${(props) => props.theme.fontSize.s16h24};
 `;
 
 // 반려동물이 없을 때
@@ -151,6 +170,14 @@ const NoPetsContainer = styled.div`
 
   & > div {
     margin-bottom: 30px;
+  }
+`;
+
+const PlusIcon = styled(AddCircleOutlineIcon)`
+  color: gray;
+  margin: 0;
+  &:hover {
+    color: #279eff;
   }
 `;
 
